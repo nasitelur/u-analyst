532e6021c9efeb784bc6d70b72dfe31a2d8901c9
Le <140475942+nasitelur@users.noreply.github.com>
2025-10-26 20:52:25 +0700

    vercel

---
+++ src/script.js
@@ -0,0 +1,8 @@
+const navbar = document.getElementById("navbar");
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add("navbar-scrolled");
+  } else {
+    navbar.classList.remove("navbar-scrolled");
+  }
+});
